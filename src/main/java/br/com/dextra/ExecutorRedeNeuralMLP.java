197ba98e70c36802e816eab71ab9470a6d15e6dc
Gago <gago@gago-Inspiron-1525.(none)>
2014-11-08 13:42:36 -0200

    Renomear classes

---
+++ src/main/java/br/com/dextra/ExecutorRedeNeuralMLP.java
@@ -0,0 +1,38 @@
+package br.com.dextra;
+
+
+public class ExecutorRedeNeuralMLP {
+
+	private static double[][] CONJUNTO_TREINAMENTO = {
+		{0.0, 0.0, 1.0, 1.0},
+		{0.0, 1.0, 0.0, 1.0},
+		{1.0, 1.0, 1.0, 1.0}
+	};
+
+	private static double[] VALORES_ESPERADOS = {0.0, 0.0, 0.0, 1.0};
+
+	public static void main(String[] args) {
+
+		int nrNeuroniosPrimeiraCamada = 3;
+		int nrNeuroniosEntrada = 3;
+
+		RedeNeuralMLP rede = new RedeNeuralMLP(nrNeuroniosPrimeiraCamada, nrNeuroniosEntrada);
+		
+		System.out.println("Teste antes do treinamento: --------------------------------------------------------------------------- \n");
+		ExecutorRedeNeuralMLP.imprimirTesteDeClassificacao(rede);
+
+		rede.treinar(ExecutorRedeNeuralMLP.CONJUNTO_TREINAMENTO, ExecutorRedeNeuralMLP.VALORES_ESPERADOS);
+
+		System.out.println("\n Teste depois do treinamento: -------------------------------------------------------------------------- \n");
+		ExecutorRedeNeuralMLP.imprimirTesteDeClassificacao(rede);
+		
+	}
+
+	private static void imprimirTesteDeClassificacao(RedeNeuralMLP rede) {
+		rede.classificar(new double[] { 0.0, 0.0, 1.0 });
+		rede.classificar(new double[] { 0.0, 1.0, 1.0 });
+		rede.classificar(new double[] { 1.0, 0.0, 1.0 });
+		rede.classificar(new double[] { 1.0, 1.0, 1.0 });
+	}
+
+}
---
+++ src/main/java/br/com/dextra/RedeNeuralParameter.java
@@ -0,0 +1,11 @@
+package br.com.dextra;
+
+public class RedeNeuralParameter {
+	public int nrNeuroniosPrimeiraCamada;
+	public int nrNeuroniosSegundaCamada;
+
+	public RedeNeuralParameter(int nrNeuroniosPrimeiraCamada, int nrNeuroniosSegundaCamada) {
+		this.nrNeuroniosPrimeiraCamada = nrNeuroniosPrimeiraCamada;
+		this.nrNeuroniosSegundaCamada = nrNeuroniosSegundaCamada;
+	}
+}
