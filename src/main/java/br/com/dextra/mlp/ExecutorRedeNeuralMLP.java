579ecf9a1774057a960453c4352d72f1da669e12
Everton Gago <everton.gago@dextra-sw.com>
2014-11-19 09:41:32 -0200

    Treino

--- src/main/java/br/com/dextra/mlp/ExecutorRedeNeuralMLP.java
+++ src/main/java/br/com/dextra/mlp/ExecutorRedeNeuralMLP.java
@@ -9,7 +9,18 @@ public class ExecutorRedeNeuralMLP {
 		{1.0, 1.0, 1.0, 1.0} // bias
 	};
 
+	/*private static double[][] CONJUNTO_TREINAMENTO = {
+		{0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0},
+		{0.0, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0},
+		{0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0},
+		{1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0},
+		{0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0},
+		{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0} // bias
+	};*/
+
 	private static double[] VALORES_ESPERADOS = {0.0, 1.0, 1.0, 0.0};
+
+	//private static double[] VALORES_ESPERADOS = {1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0};
 
 	public static void main(String[] args) {
 
@@ -33,6 +44,9 @@ public class ExecutorRedeNeuralMLP {
 		rede.classificar(new double[] { 0.0, 1.0, 1.0 });
 		rede.classificar(new double[] { 1.0, 0.0, 1.0 });
 		rede.classificar(new double[] { 1.0, 1.0, 1.0 });
+
+		/*rede.classificar(new double[] {0.0, 0.0, 0.0, 1.0, 0.0, 1.0});
+		rede.classificar(new double[] {1.0, 0.5, 1.0, 1.0, 1.0, 1.0});*/
 	}
 
 }
