690ffe9f2760855e78d079494961731107c1e2e7
Everton Gago <everton.gago@dextra-sw.com>
2016-06-16 16:00:31 -0300

    Ajustes

--- src/main/java/br/unicamp/perceptron/Executor.java
+++ src/main/java/br/unicamp/perceptron/Executor.java
@@ -1,45 +1,32 @@
 package br.unicamp.perceptron;
 
 public class Executor {
-
+	
 	private static final Double[][] AMOSTRAS = new Double[][] {
-		{0d, 0d, 1d},
-		{0d, 1d, 1d},
-		{1d, 0d, 1d},
-		{1d, 1d, 1d}
+		{ 1d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		{ 0d, 0d, 0d, 0d, 1d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		{ 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		{ 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		{ 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d },
+		{ 0d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 1d, 0d, 0d, 0d, 0d, 0d, 1d },
+		{ 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 0d, 0d, 1d, 1d, 0d, 0d, 0d, 1d },
+		{ 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 1d, 0d, 1d },
+		{ 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 1d, 1d }
     };
-
-	private static final Double[] ESPERADOS = new Double[] { 0d, 0d, 0d, 1.0d };
+    
+    private static final Double[] ESPERADOS = new Double[] { 1d, 0d, 0d, 1d, 0d, 1d, 0d, 1d, 1d };
 
 	public static void main(String[] args) {
-
+		// Treino.
 		Neuronio rede = new Neuronio(AMOSTRAS, ESPERADOS);
 		rede.treinar();
+		
+		// Teste.
+		Double s1 = rede.classificar(new Double[] { 0d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 1d, 0d, 1d, 1d });
+		Double s2 = rede.classificar(new Double[] { 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 0d, 1d, 0d, 0d, 0d, 0d, 1d });
 
-		// TODO trocar o padrao para testar.
-		Double[] padrao = {1d, 0d, 1d};
-
-		// teste.
-		Double saida = rede.classificar(padrao);
-
-		Executor.imprimirResultadosProcessamento(rede, saida);
-	}
-
-	private static void imprimirResultadosProcessamento(Neuronio rede, Double saida) {
-		StringBuilder conhecimento = new StringBuilder();
-		Double[] pesos = rede.getPesos();
-		for (int i = 0; i < pesos.length; i++) {
-			conhecimento.append("\t[");
-			conhecimento.append(i);
-			conhecimento.append("]: ");
-			conhecimento.append(pesos[i]);
-			if (i < (pesos.length - 1)) {
-				conhecimento.append("\n");
-			}
-		}
-
-		System.out.println(String.format("- O valor calculado pela rede foi %s, depois de %s epocas de treinamento.", saida.toString(), rede.getEpocas()));
-		System.out.println(String.format("- Conhecimento obtido durante a fase de treinamento: \n%s", conhecimento.toString()));
+		System.out.println(s1 + " e " + s2);
+		System.out.println(rede.getEpocas());
 	}
 
 }
